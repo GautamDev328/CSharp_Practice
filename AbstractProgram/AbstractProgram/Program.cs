a6796876db0f1498ce76e7b3bf0e5460406d1c0c
GAUTAM DEV <GautamDev328@users.noreply.github.com>
2022-07-21 00:37:55 +0530

    Abstractprogram

---
+++ AbstractProgram/AbstractProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractProgram
+{
+    public abstract class A
+    {
+        public abstract void Rond();
+    }
+    
+        public class B : A {
+            public override void Rond()
+            {
+                Console.WriteLine("20");
+            }
+        }
+        class Test {
+            static void Main()
+            {
+                A a = new B();
+                a.Rond();
+            Console.ReadLine();
+
+            }
+        }
+    }
