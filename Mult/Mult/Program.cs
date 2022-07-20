a1042567d4dd0c39acb025c138345c3c2aef306b
GAUTAM DEV <GautamDev328@users.noreply.github.com>
2022-07-20 23:57:58 +0530

    Multiplication program

---
+++ Mult/Mult/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mult
+{
+    internal class Multiplication
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            int y = 10;
+            int z = x * y;
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+    }
+}
