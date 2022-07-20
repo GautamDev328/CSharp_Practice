80bf2044a5f1fd8c04a311398f26e0f241eba89a
GAUTAM DEV <GautamDev328@users.noreply.github.com>
2022-07-20 23:49:09 +0530

    Add twoNumber

---
+++ Program/Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            int z = x + y;
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+    }
+}
