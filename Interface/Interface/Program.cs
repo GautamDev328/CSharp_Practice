381917ea3ccdc50af4ab767343aeef23e83d0e37
GAUTAM DEV <GautamDev328@users.noreply.github.com>
2022-07-22 00:48:05 +0530

    InterfaceProgram

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface Inter
+    {
+        void sum();
+    }
+    interface book {
+        void sum2();
+    }
+  
+
+    class A : Inter, book
+    {
+public  void sum()
+        {
+            Console.WriteLine("India");
+        }
+     public   void sum2()
+        {
+            Console.WriteLine("Bharat");
+        }
+
+
+        static void Main()
+        {
+            A a = new A();
+            a.sum();
+            a.sum2();
+        //    a.tution();
+            Console.ReadLine();
+        }
+    }
+
+      
+}
