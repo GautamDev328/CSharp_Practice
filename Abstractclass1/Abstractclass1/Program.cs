958fe0822f9eaebdfeada11f4ad8802bef79f428
GAUTAM DEV <GautamDev328@users.noreply.github.com>
2022-07-22 00:33:34 +0530

    Abstractprogram2

---
+++ Abstractclass1/Abstractclass1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstractclass1
+{
+    public abstract class source
+    
+    {
+        public void tution()
+        {
+            Console.WriteLine("India");
+        }
+        public abstract void Sum();
+    }
+   
+    public  class Test : source
+    {
+        public override void Sum()
+        {
+            Console.WriteLine("Hello");
+        }
+     static void Main(string[] args)
+        {
+            source s = new Test();
+            s.Sum();
+            s.tution();
+            Console.ReadLine();
+        }
+    }
+}
