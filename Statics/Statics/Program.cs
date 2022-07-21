f281f6a496285cb51997ed5ecb606dcc57f0a085
GAUTAM DEV <GautamDev328@users.noreply.github.com>
2022-07-21 23:36:14 +0530

    STATICPROGRAM2

---
+++ Statics/Statics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statics
+{
+    public static class Data { 
+    static Data()
+    {
+        Console.WriteLine("Hi");
+    }
+    static void Test()
+    {
+        Console.WriteLine("India");
+    }
+
+
+    class Book
+    {
+        static void Main()
+        {
+
+            // Program.Test();
+
+            Data.Test();
+            Console.WriteLine("I am fine");
+            Console.WriteLine("I am also fine");
+            Console.ReadLine();
+        }
+    }
+}
+}
