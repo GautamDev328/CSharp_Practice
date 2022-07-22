e940c9fbef044acba269929118632fbec5aa690a
GAUTAM DEV <GautamDev328@users.noreply.github.com>
2022-07-22 22:40:59 +0530

    static program

---
+++ StaticProgram/StaticProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticProgram
+{
+    public static class Program
+    {
+        static Program()
+        {
+            Console.WriteLine("hello india");
+        }
+        static void sum()
+        {
+            Console.WriteLine("hi");
+            Console.ReadLine();
+        }
+        static void sum2()
+        {
+            Console.WriteLine("hello");
+            Console.ReadLine();
+        }
+        static void Main()
+        {
+            Console.WriteLine("INDIA");
+            Program.sum();
+            Program.sum2();
+            Console.ReadLine();
+           
+
+        }
+
+    }
+}
