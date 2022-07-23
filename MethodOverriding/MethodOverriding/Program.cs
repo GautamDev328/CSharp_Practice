0a89c3312bbe0950afe65ae91a301f4ee64a513f
GAUTAM DEV <GautamDev328@users.noreply.github.com>
2022-07-23 13:42:57 +0530

    overloadingProgram2

---
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    public class Program
+    {
+        public virtual void sum()
+        {
+
+            Console.WriteLine("hello");
+        }
+    }
+
+    class Tution : Program
+    {
+        public override void sum()
+        {
+            Console.WriteLine("India");
+        }
+
+        static void Main(string[] args)
+        {
+            Tution t = new Tution();
+            t.sum();
+           Program p = new Tution();
+            p.sum();
+            Console.ReadLine();
+        }
+    }
+}
+
