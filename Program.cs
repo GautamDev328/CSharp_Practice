a6d8fdd5c01c89857912c22b559d5fa08d850e64
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-20 13:01:44 +0530

    Lowercase

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lowercase
+{
+    class Program
+    {
+        static void Lowerchar( )
+        {
+            string s1;
+            char ch;
+            Console.WriteLine("Enter string:");
+            s1 = Console.ReadLine();
+            int l = s1.Length;
+            for (int i = 0; i < l; i++)
+            {
+                ch = s1[i];
+                if (char.IsUpper(ch))
+
+                    Console.Write(char.ToLower(ch));
+
+                else
+                    Console.Write(ch);
+            }
+        }
+            static void Main(string[] args)
+            {
+                Lowerchar();
+                Console.ReadLine();
+            }
+        }
+    }
+
