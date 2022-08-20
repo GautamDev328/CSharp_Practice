daade64c03f3516123f301a1307e14d7db586a21
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-20 15:52:06 +0530

    Table using forloop

---
+++ Table using whileloop/Table using whilellop/Table using whilellop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table_using_whilellop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ctrl = 1;
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            while (ctrl < 10)
+            {
+                Console.WriteLine(num+"*"+ctrl+ " = "+ ctrl*num);
+
+                ctrl = ctrl + 1;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Table using whileloop/Table using whileloop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table_using_whileloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            while (i <= 20){
+                Console.WriteLine(i);
+                i = i + 2;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
