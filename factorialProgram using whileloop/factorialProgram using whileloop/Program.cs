77b4151a2ee6c13502ffd316a8fc6249f86a691e
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-20 15:46:53 +0530

    factorialProgram using whileloop

---
+++ factorialProgram using whileloop/factorialProgram using whileloop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorialProgram_using_whileloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ctrl = 1, fact = 1;
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            while (ctrl < 5)
+            {
+                fact = fact * ctrl;
+                ctrl++;
+            }
+            Console.WriteLine("fact is:"+num+"is:"+ fact);
+            Console.ReadLine();
+        }
+    }
+}
