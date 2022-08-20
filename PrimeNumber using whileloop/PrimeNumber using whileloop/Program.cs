f11b5ea14bdb6bf3f7117660bfb05cb785f03f62
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-20 15:48:33 +0530

    PrimeNumber using whileloop

---
+++ PrimeNumber using whileloop/PrimeNumber using whileloop/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber_using_whileloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 2, num, flag = 1;
+            Console.WriteLine("Enter a number");
+            num = int.Parse(Console.ReadLine());
+            int ctrl = 0;
+            int num1;
+            int count1 = 0;
+            while (ctrl > 5)
+            {
+                Console.WriteLine("Enter a number");
+                num1 = int.Parse(Console.ReadLine());
+                if (num1 * num1 * num1 > 100)
+                    //   count++
+                
+
+            while (count < num / 2)
+                    {
+                        if (num % count == 0)
+                        {
+                            Console.WriteLine("Not prime");
+                            flag = 1;
+                            break;
+
+                        }
+
+                        if (count1 == 0)
+
+
+                            Console.WriteLine("prime Number");
+
+                        count = count + 1;
+
+
+
+                    }
+                count++;
+            }
+            Console.WriteLine("count values:"+ count);
+            Console.ReadLine();
+          
+        }
+    }
+}
+
