58d889a4b43163212b76f9e7bda244b2cf5f758a
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-20 15:49:39 +0530

    able using Do

---
+++ Table using Do/Table using Do/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table_using_Do
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine(i);
+                i = i + 2;
+
+            } while (i <= 20);
+                Console.ReadLine();
+        }
+    }
+}
