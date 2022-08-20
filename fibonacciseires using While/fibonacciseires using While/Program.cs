9af22fecb9926a77cee5544fe4938e48b27ddd6c
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-20 15:47:41 +0530

    fibonacciseires using While

---
+++ fibonacciseires using While/fibonacciseires using While/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacciseires_using_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
