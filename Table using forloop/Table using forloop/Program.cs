6322fb569f2100bb5ead254f27b49e22b5ec391c
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-20 15:51:06 +0530

    Table using forloop

---
+++ Table using forloop/Table using forloop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table_using_forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            for (; i <= 20; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
