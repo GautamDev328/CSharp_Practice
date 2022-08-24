c55cdec61078d3a5143b70a65f34f00866b60ff0
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-24 20:22:41 +0530

    program

---
+++ ProbleProgram/ProbleProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbleProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 11;
+            while (i <= 21)
+            {
+                if (i != 15)
+                {
+                    Console.WriteLine(i);
+                }
+                i = i + 2;
+            }
+            Console.ReadLine();
+
+        }
+     
+    }
+}
+/* Output:
+  11
+13
+17
+19
+21
+*/
